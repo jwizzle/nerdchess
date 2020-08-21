e0242fe89d315ca5910d039993f4832a0a0f490c
Jwizzle <info@hossel.net>
2020-08-21 21:23:58 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nerdchess",  # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="j wizzle",
     author_email="info@hossel.net",
     description="A Python chess engine.",
